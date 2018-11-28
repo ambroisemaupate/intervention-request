5bc39d20501508ad29c6dbca512ab25a06788432
Ambroise Maupate <ambroise@rezo-zero.com>
2018-11-28 11:18:54 +0100

    Change copyright year.

--- src/Processor/CropResizedProcessor.php
+++ src/Processor/CropResizedProcessor.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright © 2015, Ambroise Maupate
+ * Copyright © 2018, Ambroise Maupate
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
